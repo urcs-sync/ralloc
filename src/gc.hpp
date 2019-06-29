7dbbc9ffde0680c61e078acac295ce36950eca7e
Wentao <wcai6@cs.rochester.edu>
2019-06-29 15:21:47 -0400

    some progress in gc.hpp

--- src/gc.hpp
+++ src/gc.hpp
@@ -54,9 +54,9 @@ public:
 
 		// Step 0: initialize all transient data
 		base_md->avail_sb.off.store(nullptr); // initialize avail_sb
-		// todo: partial list of each heap
 		for(int i = 0; i< MAX_SZ_IDX; i++) {
-			base_md->heaps[i]
+			// initialize partial list of each heap
+			base_md->heaps[i].partial_list.off.store(nullptr);
 		}
 
 		// Step 1: mark all accessible blocks from roots
@@ -73,14 +73,51 @@ public:
 		Descriptor* curr_desc = base_md->desc_lookup(curr_sb);
 		auto curr_blk = marked_blk.begin();
 		char* sb_end = _rgs->regions[SB_IDX]->curr_addr_ptr->load();
+		Descriptor* next_free = nullptr;
+		Descriptor* next_partial[MAX_SZ_IDX] = {nullptr};
 		for(; curr_sb < sb_end; curr_sb+=SBSIZE, curr_desc++) {
-			// todo
-
+			Anchor anchor;
+			anchor.avail = 0;
+			anchor.count = 0;
+			anchor.state = SB_EMPTY;
+			char* free_blocks_head = nullptr;
+			char* last_possible_free_block = curr_sb;
 			while (curr_blk!=marked_blk.end() && 
 					((uint64_t)curr_sb>>SB_SHIFT) == ((uint64_t)(*curr_blk)>>SB_SHIFT)) 
-			{ // curr_blk doesn't reach the end of marked_blk and curr_blk is in curr_sb
+			{ 
+				// curr_blk doesn't reach the end of marked_blk and curr_blk is in curr_sb
+				if(curr_desc->maxcount == 1) {
+					// possible a large sb
+					if((*curr_blk) == curr_sb) {
+						// in use sb
+						anchor.avail = 0;
+						anchor.count = 0;
+						anchor.state = SB_FULL;
+					}
+				} else {
+					// possible a small sb
+					for(char* free_block = last_possible_free_block; 
+						free_block < curr_blk; free_block+=curr_desc->block_size){
+						// put last_possible_free_block...(curr_blk-1) to free blk list
+						(*reinterpret_cast<pptr<char>*>(free_block)) = free_blocks_head;
+						free_blocks_head = free_block;
+						anchor.count++;
+						anchor.state = SB_PARTIAL;
+					}
+					last_possible_free_block = (*curr_blk)+curr_desc->block_size;
+				}
 				// update local desc info
 				curr_blk++;
+			}
+			if(anchor.state == SB_EMPTY) {
+				// curr_sb isn't in use
+				curr_desc->next_free.store(next_free);
+				next_free = curr_desc;
+			} else {
+				if(anchor.count == 0) {
+					anchor.avail = curr_desc->maxcount;
+					anchor.state = SB_FULL;
+				}
 			}
 			// reconstruct blk free list
 			// update real desc info
@@ -96,13 +133,13 @@ public:
 		if(UNLIKELY(!rpmalloc::_rgs->in_range(SB_IDX, addr))) 
 			return; // return if not in range
 		// Step 2: mark potential pptr
-		marked_blk.insert(addr);
+		marked_blk.insert(reinterpret_cast<char*>(addr));
 		// Step 3: construct gc_ptr<T> from ptr and call its filter_func
 		gc_ptr<T> gc_p(addr);
 		gc_p->filter_func();
 	};
 
-	std::set<void*> marked_blk;
+	std::set<char*> marked_blk;
 	std::vector<Descriptor*> free_sb;
 };
 
