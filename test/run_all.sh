577b22dc56312223068b3c412fd911e958394f7f
Wentao <wcai6@cs.rochester.edu>
2019-07-13 11:53:16 -0400

    update run_all

--- test/run_all.sh
+++ test/run_all.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+# traditional allocator benchmarks from Hoard
 ./run_threadtest.sh
 ./run_shbench.sh
 ./run_larson.sh
+# testing producer-consumer pattern
 ./run_prod-con.sh
+# testing with Mnemosyne (VAcation and Memcached)
 ./run_vacation.sh
+# testing Redis TODO
+# testing GC time consumption
+./run_resur.sh
