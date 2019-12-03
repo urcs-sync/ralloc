9eb3a13afe0802e3d89b24fc147e4eb01c6d67f3
Wentao Cai <wcai6@cs.rochester.edu>
2019-12-03 11:18:36 -0500

    add back LGPL to RegionManager.*

--- src/RegionManager.hpp
+++ src/RegionManager.hpp
@@ -16,6 +16,21 @@ limitations under the License.
 
 */ 
 
+/*
+ * (c) Copyright 2016 Hewlett Packard Enterprise Development LP
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version. This program is
+ * distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU Lesser
+ * General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 /* 
  * This is the C++ version of region_manager code from Makalu
  * https://github.com/HewlettPackard/Atlas/tree/makalu/makalu_alloc
