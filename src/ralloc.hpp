2f1f2b229fe20c12b7faaa087d7689e7735da299
Wentao Cai <wcai6@cs.rochester.edu>
2019-12-04 11:39:42 -0500

    change license to MIT

--- src/ralloc.hpp
+++ src/ralloc.hpp
@@ -1,20 +1,8 @@
 /*
-
-Copyright 2019 University of Rochester
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License. 
-
-*/ 
+ * Copyright (C) 2019 Wentao Cai. All rights reserved.
+ * Licenced under the MIT licence. See LICENSE file in the project root for
+ * details. 
+ */
 
 #ifndef _RALLOC_HPP_
 #define _RALLOC_HPP_
